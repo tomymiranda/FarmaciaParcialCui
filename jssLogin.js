c18670f8f8fdd56c8d7ab33ebcbaf3df25cc1c90
unknown <tomasmiranda98@hotmail.com>
2021-09-26 13:01:20 -0300

    se agrega validacion para usuario

--- jssLogin.js
+++ jssLogin.js
@@ -1,4 +1,21 @@
+var usuario = document.getElementById("ingresoUsuario");
+var clave = document.getElementById("claveUsuario");
+var regExp = /^([0-9])*$/;   //[0123456789]{1,8}/;
+
 function redirigir(){
     window.location.href ="pantallaPrincipal.html";
-  }
-  document.getElementById("botonIniciarSesion").addEventListener("click",redirigir);
+}
+function enviarDatos(){
+    if(!regExp.test(usuario.value)){
+      alert("el usuario ingresado debe ser un numero");
+    }
+    else if(clave.value.length < 4 ){
+      alert("el tamaño de la clave debe ser por lo menos 4");
+    }
+    else{
+      redirigir()
+    }
+}
+
+document.getElementById("botonIniciarSesion").addEventListener("click",enviarDatos);
+  
