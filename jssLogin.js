963f38c32cfaebb1277707e59434224f167e7a39
unknown <tomasmiranda98@hotmail.com>
2021-09-26 23:51:12 -0300

    mejora para validaciones

--- jssLogin.js
+++ jssLogin.js
@@ -1,21 +1,35 @@
 var usuario = document.getElementById("ingresoUsuario");
 var clave = document.getElementById("claveUsuario");
+var fomulario = document.getElementById("formulario");
 var regExp = /^([0-9])*$/;   //[0123456789]{1,8}/;
-
+/*
+var usuarioIngresado;
 function redirigir(){
     window.location.href ="pantallaPrincipal.html";
 }
-function enviarDatos(){
+function validar(){
     if(!regExp.test(usuario.value)){
       alert("el usuario ingresado debe ser un numero");
+      return false;
     }
     else if(clave.value.length < 4 ){
       alert("el tamaño de la clave debe ser por lo menos 4");
+      return false;
     }
     else{
-      redirigir()
+      return true;
     }
 }
+*/
+fomulario.addEventListener('submit', function (event) {
+  if(!regExp.test(usuario.value)){
+    alert("el usuario ingresado debe ser un numero");
+    event.preventDefault();
+  }
+  else if(clave.value.length < 4 ){
+    alert("el tamaño de la clave debe ser por lo menos 4");
+    event.preventDefault();
+  }
+});
 
-document.getElementById("botonIniciarSesion").addEventListener("click",enviarDatos);
-  
+
--- jssPrincipal.js
+++ jssPrincipal.js
@@ -1,4 +1,5 @@
 function redirigir(){
     window.location.href ="pantallaLogin.html";
   }
-  document.getElementById("botonSalir").addEventListener("click",redirigir);
+document.getElementById("usuarioLogeado").textContent = "safd";
+document.getElementById("botonSalir").addEventListener("click",redirigir);
